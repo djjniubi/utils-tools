c675a29a8c9bb62aef7ccc264b57b5e4d39ad4e8
djj <2861606659@qq.com>
2024-01-16 17:28:25 +0800

    修改package参数

--- src/modules/throttle.ts
+++ src/modules/throttle.ts
@@ -1,10 +1,17 @@
+/*
+ * @Author: 前端菜鸟--邓建军
+ * @Date: 2023-12-18 10:49:31
+ * @FilePath: \utils-tools\src\modules\throttle.ts
+ * @LastEditors: djj
+ * @LastEditTime: 2024-01-16 15:26:21
+ */
 let timer;
 let flag:boolean;
 /**
  * @param {Function} func 要执行的回调函数
  * @param {Number} wait 延时的时间
  */
-   function throttle(func:Function,wait:number){
+   function throttle(func:Function,wait:number=800){
     if(!flag){
         flag=true;
         timer=setTimeout(()=>{
